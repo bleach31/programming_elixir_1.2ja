9c43afe624da5f77a920cf01bc86f9f42c85015b
Motohiro SHIBAKAWA <c201075@gmail.com>
2019-11-11 18:56:56 +0900

    flattenできた

--- ch10/ListsAndRecursion - 6.exs
+++ ch10/ListsAndRecursion - 6.exs
@@ -1,27 +1,25 @@
 
 defmodule MyList do
     def flatten(list) do
-        _flatten(list,[])
-    end
-
-    defp _flatten([head|tail],flat) when is_list(head) do # head リストだったらこっちに入る
-        _flatten(head,flat)
-        _flatten(tail,flat)
-        IO.inspect flat
+        _flatten(Enum.reverse(list),[])
     end
 
 
-    defp _flatten([head|tail],flat) do # head リストじゃなかったらこっちに入る
+    defp _flatten([head|tail],flat) when is_list(head) do # headがリストだったらこっちに入る
+        _flatten(tail,_flatten(head,flat))
+    end
+
+    defp _flatten([head|tail],flat) do # head リストでない
         _flatten(tail,[head|flat])
     end
-
-    defp _flatten([],_flat) do
+    
+    defp _flatten([],flat) do
+        flat
     end
 
-#    defp _flatten(nolist,flat) do
+#    defp _flatten(nolist,_flat) do
 #        nolist
 #    end
-
 
 end
 

04f5d172b60ef99310c5156deb4f10fa0b146bec
bleach31 <c201075@gmail.com>
2020-02-10 17:48:52 +0900

    できた

--- ch11/StringsAndBinaries- 5.exs
+++ ch11/StringsAndBinaries- 5.exs
@@ -2,25 +2,18 @@
 defmodule MyString do
 #一番長い文字を見つけて、rjust,ljustでパディングする
 #奇数の時どうするか？どっちか多くするしかない
-    def center(list) do
-        max = _center([h|t],0)
-        _print([h|t],max)
-    end
-    defp _center([h|t],max) do
-        if max > len h
-            max = h
-        end
-        _center(t,max)
+    def center(strlist) do
+        max = Enum.max_by(strlist, &(String.length(&1))) |> String.length
+        _print(strlist,max)   
     end
 
-    defp _center([],max) do
-        max
+    defp _print([],_) do
     end
-    defp _print([h|t],max) do
-        a = String.rjust(h,max-len h/2)
-        b = String.ljust(a,max-len h/2)
-        
-        print(b)
+
+    defp _print([h|t],width) do
+        count = (width - String.length(h))/2 + String.length(h)|> trunc
+        String.pad_leading(h, count) |> IO.inspect
+        _print(t,width)        
     end    
 
 end
